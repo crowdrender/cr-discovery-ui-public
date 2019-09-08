d648d0ac0791bc89f33ab2cad960f9b88764a1f6
Chris Cook <christopher.cook@outlook.com.au>
2019-09-08 21:24:09 +1000

    3fea713

--- js/admin/emailApp/sendBatchEmail.js
+++ js/admin/emailApp/sendBatchEmail.js
@@ -1,8 +1,6 @@
 const template=`
 <div>
-  <div v-if="loadingState === null">
-    <button @click="submit">Send Batch Email</button>
-  </div>
+  <button :disabled="loadingState === 'loading'" @click="submit">Send Batch Email</button>
   <div v-if="loadingState === 'loading'">
     <p>Awaiting server response...</p>
   </div>
@@ -13,4 +11,4 @@ const template=`
     <p>{{errorMessage}}</p>
   </div>
 </div>
-`,sendBatchEmail={template:"\n<div>\n  <div v-if=\"loadingState === null\">\n    <button @click=\"submit\">Send Batch Email</button>\n  </div>\n  <div v-if=\"loadingState === 'loading'\">\n    <p>Awaiting server response...</p>\n  </div>\n  <div v-if=\"loadingState === 'sent'\">\n    <p>Email successfully sent!</p>\n  </div>\n  <div v-if=\"loadingState === 'error'\">\n    <p>{{errorMessage}}</p>\n  </div>\n</div>\n",props:["filterString","template","subject"],data:()=>({loadingState:null,errorMessage:null}),methods:{async submit(){const a=this.filterString,b={to:["{{email}}"],from:"\"CrowdRender\" <info@crowdrender.com.au>",subject:this.subject,html:this.template},c={method:"POST",body:JSON.stringify({filter:a,config:b}),headers:{Accept:"application/json","Content-Type":"application/json"}};try{this.loadingState="loading";const a=await fetch("/admin/email/send-batch",c).then(a=>a.json());if("OK"!==a.status)throw a;this.loadingState="sent"}catch(a){this.loadingState="error",this.errorMessage=JSON.stringify(a)}}}};export default sendBatchEmail;
+`,sendBatchEmail={template:"\n<div>\n  <button :disabled=\"loadingState === 'loading'\" @click=\"submit\">Send Batch Email</button>\n  <div v-if=\"loadingState === 'loading'\">\n    <p>Awaiting server response...</p>\n  </div>\n  <div v-if=\"loadingState === 'sent'\">\n    <p>Email successfully sent!</p>\n  </div>\n  <div v-if=\"loadingState === 'error'\">\n    <p>{{errorMessage}}</p>\n  </div>\n</div>\n",props:["filterString","template","subject"],data:()=>({loadingState:null,errorMessage:null}),methods:{async submit(){const a=this.filterString,b={to:["{{email}}"],from:"\"CrowdRender\" <info@crowdrender.com.au>",subject:this.subject,html:this.template},c={method:"POST",body:JSON.stringify({filter:a,config:b}),headers:{Accept:"application/json","Content-Type":"application/json"}};try{this.loadingState="loading";const a=await fetch("/admin/email/send-batch",c).then(a=>a.json());if("OK"!==a.status)throw a;this.loadingState="sent"}catch(a){this.loadingState="error",this.errorMessage=JSON.stringify(a)}}}};export default sendBatchEmail;
