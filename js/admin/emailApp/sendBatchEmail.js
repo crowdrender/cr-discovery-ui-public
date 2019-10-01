e56b460db0ccb7a01369b43bc3f239dc98289751
Chris Cook <christopher.cook@outlook.com.au>
2019-10-01 20:17:13 +1000

    808cf1a

--- js/admin/emailApp/sendBatchEmail.js
+++ js/admin/emailApp/sendBatchEmail.js
@@ -5,13 +5,13 @@ const template=`
     <p>Awaiting server response...</p>
   </div>
   <div v-if="loadingState === 'sent'">
-    <p>Email successfully sent!</p>
+    <p>{{sentCount}} emails queued</p>
   </div>
   <div v-if="loadingState === 'error'">
-    <p>{{errorMessage}}</p>
+    <p>Error: {{errorMessage}}</p>
   </div>
 </div>
-`,sendBatchEmail={template:"\n<div>\n  <button :disabled=\"loadingState === 'loading'\" @click=\"submit\">Send Batch Email</button>\n  <div v-if=\"loadingState === 'loading'\">\n    <p>Awaiting server response...</p>\n  </div>\n  <div v-if=\"loadingState === 'sent'\">\n    <p>Email successfully sent!</p>\n  </div>\n  <div v-if=\"loadingState === 'error'\">\n    <p>{{errorMessage}}</p>\n  </div>\n</div>\n",props:["filterString","template","subject"],data:()=>({loadingState:null,errorMessage:null}),methods:{async submit(){const a=this.filterString,b={to:["{{email}}"],from:"\"CrowdRender\" <info@crowdrender.com.au>",subject:this.subject,html:this.template},c={method:"POST",body:`{
+`,sendBatchEmail={template:"\n<div>\n  <button :disabled=\"loadingState === 'loading'\" @click=\"submit\">Send Batch Email</button>\n  <div v-if=\"loadingState === 'loading'\">\n    <p>Awaiting server response...</p>\n  </div>\n  <div v-if=\"loadingState === 'sent'\">\n    <p>{{sentCount}} emails queued</p>\n  </div>\n  <div v-if=\"loadingState === 'error'\">\n    <p>Error: {{errorMessage}}</p>\n  </div>\n</div>\n",props:["filterString","template","subject"],data:()=>({loadingState:null,errorMessage:null,sentCount:null}),methods:{async submit(){const a=this.filterString,b={to:["{{email}}"],from:"\"CrowdRender\" <info@crowdrender.com.au>",subject:this.subject,html:this.template},c={method:"POST",body:`{
           "filter": ${a},
           "config": ${JSON.stringify(b)}
-        }`,headers:{Accept:"application/json","Content-Type":"application/json"}};try{this.loadingState="loading";const a=await fetch("/admin/email/send-batch",c).then(a=>a.json());if("OK"!==a.status)throw a;this.loadingState="sent"}catch(a){this.loadingState="error",this.errorMessage=JSON.stringify(a)}}}};export default sendBatchEmail;
+        }`,headers:{Accept:"application/json","Content-Type":"application/json"}};try{this.loadingState="loading";const a=await fetch("/admin/email/send-batch",c).then(a=>a.json());if("OK"!==a.status)throw a;this.loadingState="sent",this.sentCount=a.count+""}catch(a){this.loadingState="error",this.errorMessage=JSON.stringify(a)}}}};export default sendBatchEmail;
