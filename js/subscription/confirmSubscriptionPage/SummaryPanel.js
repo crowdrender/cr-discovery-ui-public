b880f22bab51e5eff6555a6c570aa3e8a20e05cc
Chris Cook <christopher.cook@outlook.com.au>
2023-08-28 22:26:31 +1000

    964ef80

--- js/subscription/confirmSubscriptionPage/SummaryPanel.js
+++ js/subscription/confirmSubscriptionPage/SummaryPanel.js
@@ -4,8 +4,9 @@ import{ExpandSymbol}from"./ExpandSymbol.js";const template=html`
     <p class="subscribe-text">Subscribe to CrowdRender at</p>
     <p class="price">\${{displayPrice}}</p>
     <p class="period">per {{period}}</p>
+    <p v-if="isYearly" class="description">This yearly subscription includes the Creature Kitbash addon.</p>
     <button class="show-details text-button" @click="toggleDetails">Details <ExpandSymbol :expanded="showDetails" /></button>
     <p v-if="showDetails" class="description">{{description}}</p>
   </div>
 </div>
-`;export const SummaryPanel={template,components:{ExpandSymbol},props:{priceCents:Number,period:String,description:String},data:()=>({showDetails:!1}),methods:{toggleDetails(){this.showDetails=!this.showDetails}},computed:{displayPrice(){return(this.priceCents/100).toFixed(2)}}};function html(a,...b){return a.map((a,c)=>`${a}${b[c]||""}`.trim()).join("").trim()}
+`;export const SummaryPanel={template,components:{ExpandSymbol},props:{priceCents:Number,period:String,description:String},data:()=>({showDetails:!1}),methods:{toggleDetails(){this.showDetails=!this.showDetails}},computed:{displayPrice(){return(this.priceCents/100).toFixed(2)},isYearly(){return"year"===this.period}}};function html(a,...b){return a.map((a,c)=>`${a}${b[c]||""}`.trim()).join("").trim()}
